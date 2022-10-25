276ebd213e4a6656cdfdcc445ff4e7cd30738a74
chicodevbr <chicodevbr@gmail.com>
2022-10-25 19:12:24 -0300

    Initial Commit

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by fjunior on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
