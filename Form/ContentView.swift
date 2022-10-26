b73fa82cf6c7999305d9bca59bb2f194c3686ca0
chicodevbr <chicodevbr@gmail.com>
2022-10-25 22:19:15 -0300

    adding form

--- Form/ContentView.swift
+++ Form/ContentView.swift
@@ -10,18 +10,35 @@ import SwiftUI
 struct ContentView: View {
     @State private var emailAddress = ""
     @State private var message = "Write your message here..."
+    @State private var subject = "Help"
     @State private var includeLogs = false
     
     var body: some View {
-        Form {
-            TextField("Email Address", text: $emailAddress)
-            TextEditor(text: $message)
-            Toggle("Include Logs", isOn: $includeLogs)
-            Button("Submit", action: {
-                print("Submit button tapped")
-            })
+        NavigationView {
+            Form {
+                Section(header: Text("How can we reach you?")) {
+                    TextField("Email Address", text: $emailAddress)
+                }
+                Section(header: Text("What can we help you with?")) {
+                    Picker("Subject", selection: $subject) {
+                        ForEach(["Help", "Suggestion", "Bug Report"], id: \.self) { subject
+                            in
+                            Text(subject)
+                        }
+                    }
+                }
+                Section(header: Text("Briefly explain what's going on.")){
+                    TextEditor(text: $message)
+                }
+                Section(footer: Text("This information will be sent anonymously.")) {
+                    
+                    Toggle("Include Logs", isOn: $includeLogs)
+                }
+                Button("Submit", action: {
+                    print("Submit button tapped")
+                })
+            }
         }
-        
     }
 }
 
